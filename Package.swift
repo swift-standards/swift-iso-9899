da681c7b71cd7dd5f44bcbcb91e0ec98c8678313
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:25:49 +0100

    Update swift-standards to 0.10.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.1.0")
+        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.10.0")
     ],
     targets: [
         // C module wrapping platform math library
